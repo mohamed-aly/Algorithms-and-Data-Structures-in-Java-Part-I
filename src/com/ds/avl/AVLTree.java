bea6daa98fe0991fa98d7c615d7e71397110ac98
mohamed-aly <mali902016@gmail.com>
2021-04-19 13:38:12 +0200

    11.9. AVL implementation - implementing the rotations

--- src/com/ds/avl/AVLTree.java
+++ src/com/ds/avl/AVLTree.java
@@ -17,16 +17,42 @@ public class AVLTree<T extends Comparable<T>> implements Tree<T> {
 
     }
 
-    private int getHeight(Node<T> node){
-        if(node == null){
+    private Node<T> rotateRight(Node<T> node) {
+        Node<T> newRoot = node.getLeftChild();
+        Node<T> t = newRoot.getRightChild();
+
+        newRoot.setRightChild(node);
+        node.setLeftChild(t);
+
+        newRoot.setHeight(Math.max(getHeight(newRoot.getLeftChild()), getHeight(newRoot.getRightChild())) + 1);
+        node.setHeight(Math.max(getHeight(node.getLeftChild()), getHeight(node.getRightChild())) + 1);
+
+        return newRoot;
+    }
+
+    private Node<T> rotateLeft(Node<T> node) {
+        Node<T> newRoot = node.getRightChild();
+        Node<T> t = newRoot.getLeftChild();
+
+        newRoot.setLeftChild(node);
+        node.setRightChild(t);
+
+        newRoot.setHeight(Math.max(getHeight(newRoot.getLeftChild()), getHeight(newRoot.getRightChild())) + 1);
+        node.setHeight(Math.max(getHeight(node.getLeftChild()), getHeight(node.getRightChild())) + 1);
+
+        return newRoot;
+    }
+
+    private int getHeight(Node<T> node) {
+        if (node == null) {
             return -1;
         }
 
         return node.getHeight();
     }
 
-    private int getBalance(Node<T> node){
-        if(node == null){
+    private int getBalance(Node<T> node) {
+        if (node == null) {
             return 0;
         }
 
@@ -45,7 +71,7 @@ public class AVLTree<T extends Comparable<T>> implements Tree<T> {
 
         System.out.print(node + " ");
 
-        if(node.getRightChild() != null){
+        if (node.getRightChild() != null) {
             inOrderTraversal(node.getRightChild());
         }
     }
