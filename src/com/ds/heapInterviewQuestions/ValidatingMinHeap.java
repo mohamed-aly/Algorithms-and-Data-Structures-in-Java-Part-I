13cbdbb0c55692057e7f42781f2a6e7a9787509d
mohamed-aly <mali902016@gmail.com>
2021-06-01 14:47:41 +0200

    16.2. Checking array heap representation solution(enhanced)

--- src/com/ds/heapInterviewQuestions/ValidatingMinHeap.java
+++ src/com/ds/heapInterviewQuestions/ValidatingMinHeap.java
@@ -2,17 +2,15 @@ package com.ds.heapInterviewQuestions;
 
 public class ValidatingMinHeap {
     public static void main(String[] args) {
-        int[] array = {2, 5, 23, 210, 100};
+        int[] array = {2, 5, 23, 210, 100, 1};
         System.out.println(isValidMinHeap(array));
     }
 
     private static boolean isValidMinHeap(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i <= (array.length - 1) / 2; i++) {
             int leftChildIndex = (i * 2) + 1;
             int rightChildIndex = (i * 2) + 2;
-            if (leftChildIndex < array.length && array[i] > array[leftChildIndex]) {
-                return false;
-            } else if (rightChildIndex < array.length && array[i] > array[rightChildIndex]) {
+            if (array[i] > array[leftChildIndex] || (rightChildIndex < array.length && array[i] > array[rightChildIndex])) {
                 return false;
             }
         }
