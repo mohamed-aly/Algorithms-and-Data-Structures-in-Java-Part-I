1eea38fd325bdd895545faf0578ff66b455d811a
mohamed-aly <mali902016@gmail.com>
2021-06-02 14:04:21 +0200

    17.8. Chaining implementation II - get

--- src/com/ds/hashMap/HashTable.java
+++ src/com/ds/hashMap/HashTable.java
@@ -3,6 +3,26 @@ package com.ds.hashMap;
 public class HashTable<K, V> {
     private HashItem<K, V>[] hashTable;
 
+    public V get(K key) {
+        int hashArrayIndex = hash(key);
+        HashItem<K, V> hashItem = this.hashTable[hashArrayIndex];
+
+        if(hashItem == null){
+            throw new IllegalArgumentException("No Records Found with the provided Key");
+        }else{
+
+            while (hashItem != null && !hashItem.getKey().equals(key)){
+                hashItem = hashItem.getNextHashItem();
+            }
+
+            if(hashItem == null){
+                throw new IllegalArgumentException("No Records Found with the provided Key");
+            }
+
+            return hashItem.getValue();
+        }
+    }
+
     public void put(K key, V value) {
         int hashArrayIndex = hash(key);
 
